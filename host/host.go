eaa0fa4d2daa1eb5447a8e3c74df7b13a7fa64a4
Riften <943275366@qq.com>
2020-04-19 12:18:01 +0800

    framework update

---
+++ host/host.go
@@ -0,0 +1,13 @@
+package host
+
+import (
+	"context"
+	"github.com/libp2p/go-libp2p"
+	p2phost "github.com/libp2p/go-libp2p-core/host"
+)
+
+// Create host for shadow peer
+
+func NewHost(ctx context.Context) (p2phost.Host, error){
+	return libp2p.New(ctx)
+}
---
+++ service/shadow_service.go
@@ -0,0 +1,8 @@
+package service
+
+import"github.com/libp2p/go-libp2p-core/protocol"
+// Shadow protocal service
+// Make sure to change it to "shadow" instead of "shadow_test" before connect with normal peer
+const shadowServiceProtocol = protocol.ID("/textile/shadow_test/1.0.0")
+
+
