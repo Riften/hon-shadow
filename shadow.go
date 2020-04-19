6cd52fab6becfbf55445af5f887671da67c6c8c2
Riften <943275366@qq.com>
2020-04-19 13:35:06 +0800

    update

--- shadow.go
+++ shadow.go
@@ -4,7 +4,7 @@ import (
 	//"crypto/rand"
 	"context"
 	"fmt"
-	"github.com/Riften/hon-shadow/host"
+	host "github.com/Riften/hon-shadow/host"
 	//"github.com/libp2p/go-libp2p-core/crypto"
 )
 
@@ -16,7 +16,7 @@ type shadowPeer struct {
 // The context governs the lifetime of the libp2p node
 func main() {
 	ctx := context.Background()
-	h, err := host.NewHost(ctx)
-
+	h, err := host.NewHost(ctx);  if err != nil {fmt.Print(err.Error())}
+	
 	fmt.Printf("Hello World, my hosts ID is %s\n", h.ID())
 }
